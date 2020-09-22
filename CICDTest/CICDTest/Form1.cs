e875c5a3cf64261756992ac0b85ac1e5bab40b63
chanos <chanos.dev@gmail.com>
2020-09-23 08:38:53 +0900

    webhook push event test

--- CICDTest/CICDTest/Form1.cs
+++ CICDTest/CICDTest/Form1.cs
@@ -20,6 +20,7 @@ namespace CICDTest
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("CICDTest!!!!!!");
+            MessageBox.Show("Test WebHook!");
         }
     }
 }
--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Github repository 연동
 MSBuild plugin을 이용하여 C# 프로젝트 빌드
 
 Groovy postbuild plugin을 이용한 빌드 후 Line 알림
+
+브랜치 별로 jobs 생성
+
+Webhook을 이용한 push event and merge event시 빌드 유발
